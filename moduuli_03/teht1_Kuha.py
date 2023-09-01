7df6b76abc5985052663177ab4bd5401c14c431c
Xanpo <soyrnoel@gmail.com>
2023-09-01 13:53:23 +0300

    moduuli03_fix

--- moduuli_03/teht1_Kuha.py
+++ moduuli_03/teht1_Kuha.py
@@ -1,5 +1,5 @@
 kuha = float(input("Anna kuhan cm pituus: "))
 puuttuvaCm = 37-kuha
 
-if kuha <= 37:
+if kuha < 37:  # Ennen oli kuha <= 37.
     print(f"Laske kuha takaisin järveen, koska sen tarvii olla {puuttuvaCm} cm pidempi.")
--- moduuli_03/teht4_Karkausvuosi.py
+++ moduuli_03/teht4_Karkausvuosi.py
@@ -1,6 +1,6 @@
 vuosi = int(input("Vuosiluku: "))
 
-if vuosi % 400 == 0 and vuosi % 4 == 0 or vuosi % 100 != 0:
+if vuosi % 400 == 0 or vuosi % 4 == 0 and vuosi % 100 != 0:  # Ennen oli "or" ja "and" väärällä kohdalla.
     print(str(int(vuosi)) + " on karkausvuosi.")
 else:
     print(str(int(vuosi)) + " ei ole karkausvuosi.")
