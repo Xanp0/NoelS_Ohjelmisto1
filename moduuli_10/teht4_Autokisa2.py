8bec6fed6a8478694001d1fddabfae91b22e57ea
Xanpo <soyrnoel@gmail.com>
2023-10-27 12:24:53 +0300

    moduuli10_tehty

--- moduuli_10/teht4_Autokisa2.py
+++ moduuli_10/teht4_Autokisa2.py
@@ -1,29 +1,31 @@
 import random
-# jolla on ominaisuuksina kilpailun nimi, pituus kilometreinä ja osallistuvien autojen lista.
-# Luokassa on alustaja, joka saa parametreinaan nimen, kilometrimäärän ja autolistan ja
-# asettaa ne ominaisuuksille arvoiksi. Luokassa on seuraavat metodit:
 
-# tunti_kuluu, joka toteuttaa aiemmassa autokilpailutehtävässä mainitut tunnin välein
-# tehtävät toimenpiteet eli arpoo kunkin auton nopeuden muutoksen ja kutsuu kullekin autolle kulje-metodia.
-
-# tulosta_tilanne, joka tulostaa kaikkien autojen sen hetkiset tiedot selkeäksi taulukoksi muotoiltuna.
-
-# kilpailu_ohi, joka palauttaa True, jos jokin autoista on maalissa eli se
-# on ajanut vähintään kilpailun kokonaiskilometrimäärän. Muussa tapauksessa palautetaan False
 class Competition:
     def __init__(self, compName, raceLength, competitors):
         self.compName = compName
         self.raceLength = raceLength
         self.competitors = competitors
 
-    def timeSpent(self):
+    def timeUsed(self):
+        for racers in self.competitors:
+            speedChanges = random.uniform(-10, 15)
+            racers.kiihdytä(speedChanges)
+            racers.kulje(1)
         return
 
     def compSituation(self):
+        for y in self.competitors:
+            print(f"Rekisteri: {y.regNumber}. Huippunopeus: {y.maxSpeed:.0f} km/h. "
+                  f"Tämänhetkinen nopeus: {y.velocity:.0f} km/h. Kuljettu matka: {y.travel:.0f} km.")
         return
 
     def compEnded(self):
-        return
+        kisaLoppu = False
+        for z in self.competitors:
+            if z.travel >= self.raceLength:
+                kisaLoppu = True
+                break
+        return kisaLoppu
 
 class Car:
     def __init__(self, regNumber, maxSpeed, velocity=0, travel=0):
@@ -46,12 +48,6 @@ class Car:
         self.travel += self.velocity * travelTime
         return
 
-# pääohjelma, joka luo 8000 kilometrin kilpailun nimeltä "Suuri romuralli".
-# Luotavalle kilpailulle annetaan kymmenen auton lista samaan tapaan kuin
-# aiemmassa tehtävässä. Pääohjelma simuloi kilpailun etenemistä kutsumalla
-# toistorakenteessa tunti_kuluu-metodia, jonka jälkeen aina tarkistetaan
-# kilpailu_ohi-metodin avulla, onko kilpailu ohi. Ajantasainen tilanne tulostetaan
-# tulosta tilanne-metodin avulla kymmenen tunnin välein sekä kertaalleen sen jälkeen, kun kilpailu on päättynyt.
 autot = []
 
 for i in range(1, 11):
@@ -60,21 +56,23 @@ for i in range(1, 11):
     raceCar = Car(regNum, newMaxSpeed)
     autot.append(raceCar)
 
+kisa = Competition("Suuri romuralli", 8000, autot)
 kisaJatkuu = True
 kisaKestänyt = 0
 
 while kisaJatkuu:
     for racer in autot:
-        speedChanges = random.uniform(-10, 15)
-        racer.kiihdytä(speedChanges)
-        racer.kulje(1)
 
-        if racer.travel >= 10000:
+        kisa.timeUsed()
+        kisaKestänyt += 1
+
+        if kisa.compEnded() == True:
             kisaJatkuu = False
 
-    kisaKestänyt += 1
-print(f"Kisa on kestänyt {kisaKestänyt} tuntia.")
+        if kisaKestänyt % 10 == 0:
+            print(f"Kisan tilanne {kisaKestänyt} tunnin jälkeen:")
+            kisa.compSituation()
+            print("")
 
-for racers in autot:
-    print(f"Rekisteri: {racers.regNumber}. Huippunopeus: {racers.maxSpeed:.0f} km/h. "
-          f"Tämänhetkinen nopeus: {racers.velocity:.0f} km/h. Kuljettu matka: {racers.travel:.0f} km.")
+print(f"Kisa on päättynyt ja siinä kesti {kisaKestänyt} tuntia.")
+kisa.compSituation()
--- moduuli_11/teht2_a.py
+++ moduuli_11/teht2_a.py
@@ -7,3 +7,23 @@
 # luot yhden sähköauton (ABC-15, 180 km/h, 52.5 kWh) ja yhden polttomoottoriauton (ACD-123,
 # 165 km/h, 32.3 l). Aseta kummallekin autolle haluamasi nopeus, käske autoja
 # ajamaan kolmen tunnin verran ja tulosta autojen matkamittarilukemat
+class Car:
+    def __init__(self, regNumber, maxSpeed, velocity=0, travel=0):
+        self.regNumber = regNumber
+        self.maxSpeed = maxSpeed
+        self.velocity = velocity
+        self.travel = travel
+
+    def kiihdytä(self, speed):
+        self.velocity += speed
+
+        if self.velocity > self.maxSpeed:
+            self.velocity = self.maxSpeed
+
+        elif self.velocity < 0:
+            self.velocity = 0
+        return
+
+    def kulje(self, travelTime):
+        self.travel += self.velocity * travelTime
+        return
