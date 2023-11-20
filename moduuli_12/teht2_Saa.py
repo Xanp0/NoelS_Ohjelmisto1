6dd340c83ab1a55e2585bfddeff2fe64cd3a35f1
Xanpo <soyrnoel@gmail.com>
2023-11-20 11:24:07 +0200

    moduuli13_aloitettu

--- moduuli_12/teht2_Saa.py
+++ moduuli_12/teht2_Saa.py
@@ -4,16 +4,21 @@
 # Perehdy rajapinnan dokumentaatioon riittävästi. Palveluun rekisteröityminen on
 # tarpeen, jotta saat rajapintapyynnöissä tarvittavan API-avaimen (API key).
 # Selvitä myös, miten saat Kelvin-asteet muunnettua Celsius-asteiksi.
-
 import requests
-import json
 
-nimi = input("Anna paikkakunnan nimi: ")
-#try:
-    #pyynto = "api" + nimi
-    #vastaus = requests.get(pyynto)
-    #jsonVastaus = vastaus.json()
-    #print(jsonVastaus[lämpötila])
-#except requests.exceptions.RequestException as e:
-    #print ("Hakua ei voitu suorittaa.")
-    #print(e)
+komento = input("Anna paikkakunnan nimi: ")
+
+# Pyynnön malli: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+# Huom: kun lisäät todellisen hakuehdon, niin poista myös sulut { }, esim. q={city name} => q=helsinki
+# Nettiin lähetettävä lopullinen hakulause
+pyyntö = "api"
+
+try:
+    vastaus = requests.get(pyyntö)
+
+    # tarkistetaan että onnistuiko hakuoperaatio (200 = ok)
+    if vastaus.status_code == 200:
+        jsonVastaus = vastaus.json()
+
+except requests.exceptions.RequestException as e:
+    print("Ei onnistunut.")
---
+++ moduuli_13/teht1_Flask.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+@app.route('/alkuluku/<int:numero>')
+def alkuluku(numero):
+    #args = request.args
+    onkoAlku = True
+
+    for testaus in range(2, int(math.sqrt(numero))):
+        if numero % testaus == 0:
+            onkoAlku = False
+            break
+
+    if onkoAlku:
+        vastaus = {'"Number:"' + numero + ', "isPrime:true"'}
+    else:
+        vastaus = {'"Number:"' + numero + ', "isPrime:false"'}
+    return vastaus
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
